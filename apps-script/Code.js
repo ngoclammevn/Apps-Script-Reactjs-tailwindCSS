e4f868a4f948715a8200989771769fe45a8e16ce
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-01-10 13:19:55 +0700

    delete demo script files

--- apps-script/Code.js
+++
@@ -1,9 +0,0 @@
-function setS3Connection_()
-{
-  return showConnectionForm_();  
-}
-function showConnectionForm_()
-{
-  var html = HtmlService.createTemplateFromFile('index.html').evaluate().setWidth(800).setHeight(900);  
-  return SpreadsheetApp.getUi().showModalDialog(html, '😸 Upload file to S3 Bucket');       
-}
